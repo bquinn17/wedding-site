5caaebba0aa1ef2048b34976d0134f088c23eea7
Bryan Quinn <bryan@airspace-intelligence.com>
2025-12-07 20:52:13 -0500

    All scripts pulled out

---
+++ assets/js/lib/script_41.js
@@ -0,0 +1,9 @@
+
+    window.firstPageId = 'c1dmp'
+
+    if (window.requestCloseWelcomeScreen) {
+        window.requestCloseWelcomeScreen()
+    }
+	if (!window.__browser_deprecation__) {
+		window.fedops.phaseStarted('partially_visible', {paramsOverrides: { pageId: firstPageId, isSuccessfulSSR: !clientSideRender }})
+	}
