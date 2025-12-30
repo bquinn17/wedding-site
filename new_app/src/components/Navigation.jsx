8001ed189bdf3e00c0a438e197ea5b7afc684b77
Bryan Quinn <bryan@airspace-intelligence.com>
2025-12-30 13:58:32 -0500

    Update font sizes and all caps

--- new_app/src/components/Navigation.jsx
+++ new_app/src/components/Navigation.jsx
@@ -9,10 +9,10 @@ function Navigation() {
   return (
     <nav className="navigation">
       <ul>
-        <li><a href="#home" onClick={(e) => { e.preventDefault(); scrollToSection('home'); }}>HOME</a></li>
-        <li><a href="#venue" onClick={(e) => { e.preventDefault(); scrollToSection('venue'); }}>THE VENUE</a></li>
-        <li><a href="#wedding" onClick={(e) => { e.preventDefault(); scrollToSection('wedding'); }}>THE WEDDING</a></li>
-        <li><a href="#getting-there" onClick={(e) => { e.preventDefault(); scrollToSection('getting-there'); }}>GETTING THERE</a></li>
+        <li><a href="#home" onClick={(e) => { e.preventDefault(); scrollToSection('home'); }}>Home</a></li>
+        <li><a href="#venue" onClick={(e) => { e.preventDefault(); scrollToSection('venue'); }}>The Venue</a></li>
+        <li><a href="#wedding" onClick={(e) => { e.preventDefault(); scrollToSection('wedding'); }}>The Wedding</a></li>
+        <li><a href="#getting-there" onClick={(e) => { e.preventDefault(); scrollToSection('getting-there'); }}>Getting There</a></li>
         <li><a href="#rsvp" onClick={(e) => { e.preventDefault(); scrollToSection('rsvp'); }}>RSVP</a></li>
       </ul>
     </nav>
