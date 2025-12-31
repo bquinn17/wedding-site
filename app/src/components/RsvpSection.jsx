51a5446fb2f4cda6de5be42aade490e08c88e740
Bryan Quinn <bryan@airspace-intelligence.com>
2025-12-31 11:33:13 -0500

    Better top padding

--- app/src/components/RsvpSection.jsx
+++ app/src/components/RsvpSection.jsx
@@ -4,7 +4,7 @@ function RsvpSection() {
             <div className="rsvp-content">
                 <h2 className="section-title fade-in">RSVP</h2>
                 <p className="info-text fade-in-up">RSVP information will be coming closer to the wedding date. We will send out
-                    another email once the RSVP is available. If you would like to use a different email address that
+                    another email once the RSVP is available. If you would like to use a different email address from
                     the one that received this link from, please reach out to Leah or Bryan!</p>
             </div>
         </section>
