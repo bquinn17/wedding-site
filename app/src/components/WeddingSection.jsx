9314711d6f4f2b35352374b2a7e3efcffbc5efab
Bryan Quinn <bryan@airspace-intelligence.com>
2025-12-31 11:22:20 -0500

    Cleaned up padding and google maps embed

--- app/src/components/WeddingSection.jsx
+++ app/src/components/WeddingSection.jsx
@@ -18,10 +18,23 @@ function WeddingSection() {
               </a>
             </p>
             <p className="detail-text">190 W Shore Rd, South Hero, VT 05486</p>
+            <div className="map-container fade-in-up">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.5216431875197!2d-73.333023!3d44.626801!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cca6a4d7f5d9b67%3A0xa3dea3647a8df8d6!2sSnow%20Farm%20Vineyard%20%26%20Distillery!5e1!3m2!1sen!2sus!4v1767197076904!5m2!1sen!2sus"
+                width="600"
+                height="450"
+                style={{border: 0}}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Snow Farm Vineyard Location"
+              ></iframe>
+            </div>
           </div>
         </div>
 
-        <img src="/assets/images/9c608a_47d2926591aa4e8da5ff30adbc4303a8~mv2.png" alt="Decorative element" className="heart-icon zoom-in" />
+        <img src="/assets/images/9c608a_47d2926591aa4e8da5ff30adbc4303a8~mv2.png" alt="Decorative element"
+             className="heart-icon zoom-in"/>
       </div>
     </section>
   );
